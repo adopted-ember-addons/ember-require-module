2d2833bfcd7a0c2c6913ca0a6fb8d6bc84ca11c9
Robert Jackson <rwjblue@twitch.tv>
2016-11-03 19:50:48 -0400

    Allow requiring named exports.

--- addon/index.js
+++ addon/index.js
@@ -1,12 +1,12 @@
 /* globals self */
 
-export default function requireModule(module) {
+export default function requireModule(module, exportName = 'default') {
   let rjs = self.requirejs;
 
   if (
     (rjs.has && rjs.has(module)) ||
     (!rjs.has && (rjs.entries[module] || rjs.entries[`${module}/index`]))
   ) {
-    return self.require(module).default;
+    return self.require(module)[exportName];
   }
 }
--- tests/unit/require-module-test.js
+++ tests/unit/require-module-test.js
@@ -1,17 +1,26 @@
 import { module, test } from 'qunit';
+import Ember from 'ember';
 import requireModule from 'ember-require-module';
 
 module('Unit | Util | requireModule');
 
-test('requires correct module', function(assert) {
+test('requires correct module using default export', function(assert) {
   assert.expect(1);
 
   try {
-    assert.ok(requireModule('ember'));
+    assert.equal(requireModule('ember'), Ember);
   } catch (e) {
     // Should never get here
     assert.ok(false);
   }
+});
+
+test('requires correct module using named export', function(assert) {
+  assert.expect(3);
+
+  assert.equal(requireModule('dummy/tests/fixtures/sample-module', 'FOO'), 'FOO');
+  assert.equal(requireModule('dummy/tests/fixtures/sample-module', 'BAR'), 'BAR');
+  assert.equal(requireModule('dummy/tests/fixtures/sample-module', 'DERP'), undefined);
 });
 
 test('requiring incorrect module silently fails', function(assert) {
