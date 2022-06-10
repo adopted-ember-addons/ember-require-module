a63417db1b9e9e75ce93e77129c57166f0a897b3
Manuel Wiedenmann <manuel@funkensturm.de>
2022-06-10 17:26:09 +0200

    Run yarn lint:fix

--- tests/unit/require-module-test.js
+++ tests/unit/require-module-test.js
@@ -4,26 +4,35 @@ import requireModule from 'ember-require-module';
 
 module('Unit | Util | requireModule');
 
-test('requires correct module using default export', function(assert) {
+test('requires correct module using default export', function (assert) {
   assert.expect(1);
 
   try {
-    assert.equal(requireModule('ember'), Ember);
+    assert.strictEqual(requireModule('ember'), Ember);
   } catch (e) {
     // Should never get here
     assert.ok(false);
   }
 });
 
-test('requires correct module using named export', function(assert) {
+test('requires correct module using named export', function (assert) {
   assert.expect(3);
 
-  assert.equal(requireModule('dummy/tests/fixtures/sample-module', 'FOO'), 'FOO');
-  assert.equal(requireModule('dummy/tests/fixtures/sample-module', 'BAR'), 'BAR');
-  assert.equal(requireModule('dummy/tests/fixtures/sample-module', 'DERP'), undefined);
+  assert.strictEqual(
+    requireModule('dummy/tests/fixtures/sample-module', 'FOO'),
+    'FOO'
+  );
+  assert.strictEqual(
+    requireModule('dummy/tests/fixtures/sample-module', 'BAR'),
+    'BAR'
+  );
+  assert.strictEqual(
+    requireModule('dummy/tests/fixtures/sample-module', 'DERP'),
+    undefined
+  );
 });
 
-test('requiring incorrect module silently fails', function(assert) {
+test('requiring incorrect module silently fails', function (assert) {
   assert.expect(1);
 
   try {
