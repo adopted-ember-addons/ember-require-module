c0c9107ca17d629e47b71e4e2071227a8c54b7ff
Manuel Wiedenmann <manuel@funkensturm.de>
2022-06-24 15:50:09 +0200

    Update ember cli to 4.4 (#9)
    * Run ember-cli-update --to 4.4
    
    * Rename repo urls
    
    * Run yarn lint:fix
    
    * Move ember-cli-htmlbars to devDependencies
    
    * Try to fix failing test

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ember-require-module
 
-[![Build Status](https://travis-ci.org/offirgolan/ember-require-module.svg)](https://travis-ci.org/offirgolan/ember-require-module)
+[![Build Status](https://github.com/rwwagner90/ember-require-module/actions/workflows/ci.yml/badge.svg)](https://github.com/rwwagner90/ember-require-module/actions/workflows/ci.yml)
+[![Ember Observer Score](https://emberobserver.com/badges/ember-require-module.svg)](https://emberobserver.com/addons/ember-require-module)
 [![npm version](https://badge.fury.io/js/ember-require-module.svg)](http://badge.fury.io/js/ember-require-module)
 
 Dynamically require modules
@@ -11,12 +12,11 @@ Dynamically require modules
 ember install ember-require-module
 ```
 
-## Helpful Links
-
-- ### [Changelog](CHANGELOG.md)
 
 ## Looking for help?
-If it is a bug [please open an issue on GitHub](http://github.com/offirgolan/ember-require-module/issues).
+
+If it is a bug [please open an issue on GitHub](http://github.com/rwwagner90/ember-require-module/issues).
+
 
 ## Usage
 
@@ -41,3 +41,22 @@ function runTask(taskName, ...params) {
 // import a named export from a module
 const THING = requireModule('path-to-module', 'THING');
 ```
+
+
+## Compatibility
+
+See the [Changelog](CHANGELOG.md)
+
+* Ember.js v3.24 or above
+* Ember CLI v3.24 or above
+* Node.js v12 or above
+
+
+## Contributing
+
+See the [Contributing](CONTRIBUTING.md) guide for details.
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md).
--- tests/unit/require-module-test.js
+++ tests/unit/require-module-test.js
@@ -1,29 +1,38 @@
-import Ember from 'ember';
+import DS from 'ember-data'; // eslint-disable-line ember/use-ember-data-rfc-395-imports
 import { module, test } from 'qunit';
 import requireModule from 'ember-require-module';
 
 module('Unit | Util | requireModule');
 
-test('requires correct module using default export', function(assert) {
+test('requires correct module using default export', function (assert) {
   assert.expect(1);
 
   try {
-    assert.equal(requireModule('ember'), Ember);
+    assert.strictEqual(requireModule('ember-data'), DS);
   } catch (e) {
     // Should never get here
     assert.ok(false);
   }
 });
 
-test('requires correct module using named export', function(assert) {
+test('requires correct module using named export', function (assert) {
   assert.expect(3);
 
-  assert.equal(requireModule('dummy/tests/fixtures/sample-module', 'FOO'), 'FOO');
-  assert.equal(requireModule('dummy/tests/fixtures/sample-module', 'BAR'), 'BAR');
-  assert.equal(requireModule('dummy/tests/fixtures/sample-module', 'DERP'), undefined);
+  assert.strictEqual(
+    requireModule('dummy/tests/fixtures/sample-module', 'FOO'),
+    'FOO'
+  );
+  assert.strictEqual(
+    requireModule('dummy/tests/fixtures/sample-module', 'BAR'),
+    'BAR'
+  );
+  assert.strictEqual(
+    requireModule('dummy/tests/fixtures/sample-module', 'DERP'),
+    undefined
+  );
 });
 
-test('requiring incorrect module silently fails', function(assert) {
+test('requiring incorrect module silently fails', function (assert) {
   assert.expect(1);
 
   try {
